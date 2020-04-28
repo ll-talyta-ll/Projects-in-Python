cd1431853eef2f7682414ecd0f9e032f5601e9e9
ll-talyta-ll <64484600+ll-talyta-ll@users.noreply.github.com>
2020-04-28 12:09:13 -0300

    Add files via upload

---
+++ 2 b.py
@@ -0,0 +1,8 @@
+n = int(input("digite um número: "))
+qtde=0
+i=0
+for i in range(1,5,1):
+    qtde = qtde + 1
+    print("a quantidade de números digitados é: ", qtde)
+    n = int(input("digite outro número: "))
+n=0
